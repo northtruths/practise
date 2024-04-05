2a096fd618985dd8d0a8d905872db0c865a3919b
northtruths <zp1716120350@gmail.com>
2024-04-05 12:45:23 +0800

    微小修改

--- common_sort/test.c
+++ common_sort/test.c
@@ -121,7 +121,6 @@ void TestOP()
 		a6[i] = a1[i];
 		a7[i] = a1[i];
 		a8[i] = a1[i];
-		a9[i] = a1[i];
 	}
 
 	int begin1 = clock();
@@ -133,7 +132,7 @@ void TestOP()
 	int end7 = clock();
 
 	int begin2 = clock();
-	//ShellSort(a2, N);
+	ShellSort(a2, N);
 	int end2 = clock();
 
 	int begin3 = clock();
@@ -149,9 +148,12 @@ void TestOP()
 	int end5 = clock();
 
 	int begin6 = clock();
-	MergeSort(a6, N);
+	//MergeSort(a6, N);
 	int end6 = clock();
 
+	int begin8 = clock();
+	CountSort(a8, N);
+	int end8 = clock();
 
 	printf("InsertSort:%d\n", end1 - begin1);
 	printf("BubbleSort:%d\n", end7 - begin7);
@@ -160,6 +162,8 @@ void TestOP()
 	printf("HeapSort:%d\n", end4 - begin4);
 	printf("QuickSort:%d\n", end5 - begin5);
 	printf("MergeSort:%d\n", end6 - begin6);
+	printf("CountSort:%d\n", end8 - begin8);
+
 
 	free(a1);
 	free(a2);
@@ -168,13 +172,14 @@ void TestOP()
 	free(a5);
 	free(a6);
 	free(a7);
+	free(a8);
 }
 
 int main()
 {
 	//TestOP();
 
-	//InsertSortTest();
+	InsertSortTest();
 	//ShellSortTest();
 	//SelectSortTest();
 	//HeapSortTest();
@@ -183,6 +188,6 @@ int main()
 	//QuickSortNonRTest();
 	//MergeSortTest();
 	//MergeSortNonRTest();
-	CountSortTest();
+	//CountSortTest();
 	return 0;
 }
