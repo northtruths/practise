c745a65401369482cf4c3a3e152276c4871c798a
northtruths <zp1716120350@gmail.com>
2024-04-22 22:01:50 +0800

    一个小小的问题

--- ImplementDataClass/Data.h
+++ ImplementDataClass/Data.h
@@ -9,7 +9,7 @@ public:
 		int arr[13] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
 		if (month == 2 && (year % 4 == 0 && year % 100 != 0 || year % 400 == 0))
 		{
-			return 28;
+			return 29;
 		}
 		else
 		{
