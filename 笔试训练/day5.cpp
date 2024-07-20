f53db1a262fbc82dcedae7bd7d411c2deee93674
northtruths <zp1716120350@gmail.com>
2024-07-20 23:43:51 +0800

    day6前两题

--- 笔试训练/day5.cpp
+++ 笔试训练/day5.cpp
@@ -33,249 +33,253 @@
 //}
 
 
-
-#include<iostream>
-#include<vector>
-using namespace std;
-
-class Solution {
-public:
-    /**
-     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
-     *
-     *
-     * @param grid int整型vector<vector<>>
-     * @return int整型
-     */
-    bool juge(vector<vector<int> >& grid, int i, int j, int row, int col)
-    {
-        if (i == 0 || j == 0 || i == row - 1 || j == col - 1)
-        {
-            if ((i == 0 && j == 0) || (i == row - 1 && j == col - 1))
-            {
-                if (i == 0 && j == 0)
-                    return !grid[i + 1][j] && !grid[i][j + 1];
-                else
-                    return !grid[i - 1][j] && !grid[i][j - 1];
-            }
-            else if ((i == 0 && j == col - 1) || (i == row - 1 && j == 0))
-            {
-                if (i == 0 && j == col - 1)
-                    return !grid[i + 1][j] && !grid[i][j - 1];
-                else
-                    return !grid[i - 1][j] && !grid[i][j + 1];
-            }
-            else if (i == 0)
-            {
-                return !grid[i + 1][j] && !grid[i][j - 1] && !grid[i][j + 1];
-            }
-            else if (i == row - 1)
-            {
-                return !grid[i - 1][j] && !grid[i][j - 1] && !grid[i][j + 1];
-            }
-            else if (j == 0)
-            {
-                return !grid[i - 1][j] && !grid[i + 1][j] && !grid[i][j + 1];
-            }
-            else {
-                return !grid[i - 1][j] && !grid[i + 1][j] && !grid[i][j - 1];
-            }
-        }
-        else
-        {
-            return !grid[i - 1][j] && !grid[i + 1][j] && !grid[i][j - 1] && !grid[i][j + 1];
-        }
-    }
-
-    bool spread(vector<vector<int> >& grid, int i, int j, int row, int col, int& time)
-    {
-        int flag = 0;//记录是否传播
-        if ((i == 0 && j == 0) || (i == row - 1 && j == col - 1))
-        {
-            if (i == 0 && j == 0)
-            {
-                if (grid[i + 1][j] == 1)
-                {
-                    grid[i + 1][j] = time + 2;
-                    flag = 1;
-                }
-                if (grid[i][j + 1] == 1)
-                {
-                    grid[i][j + 1] = time + 2;
-                    flag = 1;
-                }
-            }
-            else
-            {
-                if (grid[i - 1][j] == 1)
-                {
-                    grid[i - 1][j] = time + 2;
-                    flag = 1;
-                }
-                if (grid[i][j - 1] == 1)
-                {
-                    grid[i][j - 1] = time + 2;
-                    flag = 1;
-                }
-            }
-        }
-        else if ((i == 0 && j == col - 1) || (i == row - 1 && j == 0))
-        {
-            if (i == 0 && j == col - 1)
-            {
-                if (grid[i + 1][j] == 1)
-                {
-                    grid[i + 1][j] = time + 2;
-                    flag = 1;
-                }
-                if (grid[i][j - 1] == 1)
-                {
-                    grid[i][j - 1] = time + 2;
-                    flag = 1;
-                }
-            }
-            else
-            {
-                if (grid[i - 1][j] == 1)
-                {
-                    grid[i - 1][j] = time + 2;
-                    flag = 1;
-                }
-                if (grid[i][j + 1] == 1)
-                {
-                    grid[i][j + 1] = time + 2;
-                    flag = 1;
-                }
-            }
-        }
-        else if (i == 0)
-        {
-            if (grid[i + 1][j] == 1)
-            {
-                grid[i + 1][j] = time + 2;
-                flag = 1;
-            }
-            if (grid[i][j + 1] == 1)
-            {
-                grid[i][j + 1] = time + 2;
-                flag = 1;
-            }
-            if (grid[i][j - 1] == 1)
-            {
-                grid[i][j - 1] = time + 2;
-                flag = 1;
-            }
-        }
-        else if (i == row - 1)
-        {
-            if (grid[i - 1][j] == 1)
-            {
-                grid[i - 1][j] = time + 2;
-                flag = 1;
-            }
-            if (grid[i][j + 1] == 1)
-            {
-                grid[i][j + 1] = time + 2;
-                flag = 1;
-            }
-            if (grid[i][j - 1] == 1)
-            {
-                grid[i][j - 1] = time + 2;
-                flag = 1;
-            }
-        }
-        else if (j == 0)
-        {
-            if (grid[i - 1][j] == 1)
-            {
-                grid[i - 1][j] = time + 2;
-                flag = 1;
-            }
-            if (grid[i + 1][j] == 1)
-            {
-                grid[i + 1][j] = time + 2;
-                flag = 1;
-            }
-            if (grid[i][j + 1] == 1)
-            {
-                grid[i][j + 1] = time + 2;
-                flag = 1;
-            }
-        }
-        else
-        {
-            if (grid[i - 1][j] == 1)
-            {
-                grid[i - 1][j] = time + 2;
-                flag = 1;
-            }
-            if (grid[i + 1][j] == 1)
-            {
-                grid[i + 1][j] = time + 2;
-                flag = 1;
-            }
-            if (grid[i][j - 1] == 1)
-            {
-                grid[i][j - 1] = time + 2;
-                flag = 1;
-            }
-        }
-        return flag;
-    }
-
-    int rotApple(vector<vector<int> >& grid) {
-        int row = grid.size();
-        int col = grid[0].size();
-        int time = 1;//传播的第几次
-        int flag = 0;//这一遍是否传播
-        while(1)
-        {
-
-            for (int i = 0; i < row; ++i)
-            {
-                for (int j = 0; j < col; ++j)
-                {
-                    if (grid[i][j] == 1)
-                    {
-                        if (juge(grid, i, j, row, col))
-                            return -1;
-                    }
-                    if (grid[i][j] == time + 1)
-                    {
-                        if (spread(grid, i, j, row, col, time) == false)
-                        {
-                            return time - 1;
-                        }
-                    }
-                }
-            }
-            ++time;
-        }
-        return time - 1;
-    }
-
-
-};
-
-
-int main()
-{
-    Solution s1;
-    int n = 0;
-    vector<vector<int>> grid(3);
-    for (int i = 0; i < 3; ++i)
-    {
-        for (int j = 0; j < 3; ++j)
-        {
-            int a = 0;
-            cin >> a;
-            grid[i].push_back(a);
-        }
-    }
-    cout << s1.rotApple(grid) << endl;
-    return 0;
-}
-
-//2 1 1 
-//1 0 1 
-//1 1 1
+//腐烂的苹果（传播）
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//
+//class Solution {
+//public:
+//    /**
+//     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+//     *
+//     *
+//     * @param grid int整型vector<vector<>>
+//     * @return int整型
+//     */
+//    bool juge(vector<vector<int> >& grid, int i, int j, int row, int col)
+//    {
+//        if (i == 0 || j == 0 || i == row - 1 || j == col - 1)
+//        {
+//            if ((i == 0 && j == 0) || (i == row - 1 && j == col - 1))
+//            {
+//                if (i == 0 && j == 0)
+//                    return !grid[i + 1][j] && !grid[i][j + 1];
+//                else
+//                    return !grid[i - 1][j] && !grid[i][j - 1];
+//            }
+//            else if ((i == 0 && j == col - 1) || (i == row - 1 && j == 0))
+//            {
+//                if (i == 0 && j == col - 1)
+//                    return !grid[i + 1][j] && !grid[i][j - 1];
+//                else
+//                    return !grid[i - 1][j] && !grid[i][j + 1];
+//            }
+//            else if (i == 0)
+//            {
+//                return !grid[i + 1][j] && !grid[i][j - 1] && !grid[i][j + 1];
+//            }
+//            else if (i == row - 1)
+//            {
+//                return !grid[i - 1][j] && !grid[i][j - 1] && !grid[i][j + 1];
+//            }
+//            else if (j == 0)
+//            {
+//                return !grid[i - 1][j] && !grid[i + 1][j] && !grid[i][j + 1];
+//            }
+//            else {
+//                return !grid[i - 1][j] && !grid[i + 1][j] && !grid[i][j - 1];
+//            }
+//        }
+//        else
+//        {
+//            return !grid[i - 1][j] && !grid[i + 1][j] && !grid[i][j - 1] && !grid[i][j + 1];
+//        }
+//    }
+//
+//    bool spread(vector<vector<int> >& grid, int i, int j, int row, int col, int& time)
+//    {
+//        int flag = 0;//记录是否传播
+//        if ((i == 0 && j == 0) || (i == row - 1 && j == col - 1))
+//        {
+//            if (i == 0 && j == 0)
+//            {
+//                if (grid[i + 1][j] == 1)
+//                {
+//                    grid[i + 1][j] = time + 2;
+//                    flag = 1;
+//                }
+//                if (grid[i][j + 1] == 1)
+//                {
+//                    grid[i][j + 1] = time + 2;
+//                    flag = 1;
+//                }
+//            }
+//            else
+//            {
+//                if (grid[i - 1][j] == 1)
+//                {
+//                    grid[i - 1][j] = time + 2;
+//                    flag = 1;
+//                }
+//                if (grid[i][j - 1] == 1)
+//                {
+//                    grid[i][j - 1] = time + 2;
+//                    flag = 1;
+//                }
+//            }
+//        }
+//        else if ((i == 0 && j == col - 1) || (i == row - 1 && j == 0))
+//        {
+//            if (i == 0 && j == col - 1)
+//            {
+//                if (grid[i + 1][j] == 1)
+//                {
+//                    grid[i + 1][j] = time + 2;
+//                    flag = 1;
+//                }
+//                if (grid[i][j - 1] == 1)
+//                {
+//                    grid[i][j - 1] = time + 2;
+//                    flag = 1;
+//                }
+//            }
+//            else
+//            {
+//                if (grid[i - 1][j] == 1)
+//                {
+//                    grid[i - 1][j] = time + 2;
+//                    flag = 1;
+//                }
+//                if (grid[i][j + 1] == 1)
+//                {
+//                    grid[i][j + 1] = time + 2;
+//                    flag = 1;
+//                }
+//            }
+//        }
+//        else if (i == 0)
+//        {
+//            if (grid[i + 1][j] == 1)
+//            {
+//                grid[i + 1][j] = time + 2;
+//                flag = 1;
+//            }
+//            if (grid[i][j + 1] == 1)
+//            {
+//                grid[i][j + 1] = time + 2;
+//                flag = 1;
+//            }
+//            if (grid[i][j - 1] == 1)
+//            {
+//                grid[i][j - 1] = time + 2;
+//                flag = 1;
+//            }
+//        }
+//        else if (i == row - 1)
+//        {
+//            if (grid[i - 1][j] == 1)
+//            {
+//                grid[i - 1][j] = time + 2;
+//                flag = 1;
+//            }
+//            if (grid[i][j + 1] == 1)
+//            {
+//                grid[i][j + 1] = time + 2;
+//                flag = 1;
+//            }
+//            if (grid[i][j - 1] == 1)
+//            {
+//                grid[i][j - 1] = time + 2;
+//                flag = 1;
+//            }
+//        }
+//        else if (j == 0)
+//        {
+//            if (grid[i - 1][j] == 1)
+//            {
+//                grid[i - 1][j] = time + 2;
+//                flag = 1;
+//            }
+//            if (grid[i + 1][j] == 1)
+//            {
+//                grid[i + 1][j] = time + 2;
+//                flag = 1;
+//            }
+//            if (grid[i][j + 1] == 1)
+//            {
+//                grid[i][j + 1] = time + 2;
+//                flag = 1;
+//            }
+//        }
+//        else
+//        {
+//            if (grid[i - 1][j] == 1)
+//            {
+//                grid[i - 1][j] = time + 2;
+//                flag = 1;
+//            }
+//            if (grid[i + 1][j] == 1)
+//            {
+//                grid[i + 1][j] = time + 2;
+//                flag = 1;
+//            }
+//            if (grid[i][j - 1] == 1)
+//            {
+//                grid[i][j - 1] = time + 2;
+//                flag = 1;
+//            }
+//        }
+//        return flag;
+//    }
+//
+//    int rotApple(vector<vector<int> >& grid) {
+//        int row = grid.size();
+//        int col = grid[0].size();
+//        int time = 1;//传播的第几次
+//        int flag = 0;//这一遍是否传播
+//        while(1)
+//        {
+//            flag = 0;
+//            for (int i = 0; i < row; ++i)
+//            {
+//                for (int j = 0; j < col; ++j)
+//                {
+//                    if (grid[i][j] == 1)
+//                    {
+//                        if (juge(grid, i, j, row, col))
+//                            return -1;
+//                    }
+//                    if (grid[i][j] == time + 1)
+//                    {
+//                        if (spread(grid, i, j, row, col, time))
+//                        {
+//                            flag = 1;
+//                        }
+//                    }
+//                }
+//            }
+//            if (flag == 0)
+//            {
+//                return time - 1;
+//            }
+//            ++time;
+//        }
+//        return time - 1;
+//    }
+//
+//
+//};
+//
+//
+//int main()
+//{
+//    Solution s1;
+//    int n = 0;
+//    vector<vector<int>> grid(3);
+//    for (int i = 0; i < 3; ++i)
+//    {
+//        for (int j = 0; j < 3; ++j)
+//        {
+//            int a = 0;
+//            cin >> a;
+//            grid[i].push_back(a);
+//        }
+//    }
+//    cout << s1.rotApple(grid) << endl;
+//    return 0;
+//}
+//
+////2 1 1 
+////1 0 1 
+////1 1 1
---
+++ 笔试训练/day6.cpp
@@ -0,0 +1,146 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//#include<iostream>
+//#include<string>
+//#include<cmath>
+//using namespace std;
+//
+//
+//string solve(string s, string t) {
+//    string& long_s = s.size() > t.size() ? s : t;
+//    string& short_s = s.size() > t.size() ? t : s;
+//
+//    string ret;
+//    int add = 0;
+//    reverse(s.begin(), s.end());
+//    reverse(t.begin(), t.end());
+//
+//    for (int i = 0; i < short_s.size(); ++i)
+//    {
+//        int a = s[i] - 48;
+//        int b = t[i] - 48;
+//        ret += (a + b + add) % 10;
+//        add = (a + b + add) / 10;
+//    }
+//    for (int i = short_s.size(); i < long_s.size(); ++i)
+//    {
+//        int a = long_s[i] - 48;
+//        ret += (a + add) % 10;
+//        add = (a + add) / 10;
+//    }
+//    if (add != 0)
+//    {
+//        ret += add;
+//    }
+//    reverse(ret.begin(), ret.end());
+//    for (auto& e : ret)
+//    {
+//        e += 48;
+//    }
+//    return ret;
+//}
+//int main()
+//{
+//	string s1;
+//    string s2;
+//    cin >> s1 >> s2;
+//    cout << solve(s1, s2) << endl;
+//
+//	return 0;
+//}
+
+
+
+
+#include<iostream>
+#include<list>
+using namespace std;
+
+
+
+
+ struct ListNode {
+ int val;
+ struct ListNode *next;
+ ListNode(int x) : val(x), next(nullptr) {}
+ };
+//class Solution {
+//public:
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     *
+     * @param head1 ListNode类
+     * @param head2 ListNode类
+     * @return ListNode类
+     */
+    ListNode* addInList(ListNode* head1, ListNode* head2) {
+        list<int, allocator<int>> ret;
+        list<int, allocator<int>> l1;
+        list<int, allocator<int>> l2;
+        while (head1)
+        {
+            l1.push_back(head1->val);
+            head1 = head1->next;
+        }
+        while (head2)
+        {
+            l2.push_back(head2->val);
+            head2 = head2->next;
+        }
+        reverse(l1.begin(), l1.end());
+        reverse(l2.begin(), l2.end());
+        list<int, allocator<int>>& long_list = l1.size() > l2.size() ? l1 : l2;
+        list<int, allocator<int>>& short_list = l1.size() > l2.size() ? l2 : l1;
+        int add = 0;
+        while(!short_list.empty())
+        {
+            int a = long_list.front();
+            int b = short_list.front();
+            ret.push_back((a + b + add) % 10);
+            add = (a + b + add) / 10;
+            long_list.pop_front();
+            short_list.pop_front();
+        }
+        while (!long_list.empty())
+        {
+            int a = long_list.front();
+            ret.push_back((a + add) % 10);
+            add = (a + add) / 10;
+            long_list.pop_front();
+        }
+        if (add != 0)
+        {
+            ret.push_back(add);
+        }
+        reverse(ret.begin(), ret.end());
+        ListNode* ret2 = (ListNode*)malloc(sizeof(ListNode));
+        ListNode* cur = ret2;
+        for (auto& e : ret)
+        {
+            cur->val = e;
+            cur->next = (ListNode*)malloc(sizeof(ListNode));
+            cur = cur->next;
+        }
+        cur = nullptr;
+        return ret2;
+    }
+//};
+int main()
+{
+    ListNode* head1 = (ListNode*)malloc(sizeof(ListNode));
+    head1->val = 9;
+    head1->next = (ListNode*)malloc(sizeof(ListNode));
+    head1->next->val = 3;
+    head1->next->next = (ListNode*)malloc(sizeof(ListNode));
+    head1->next->next->val = 7;
+    head1->next->next->next = nullptr;
+    ListNode* head2 = (ListNode*)malloc(sizeof(ListNode));
+    head2->val = 6;
+    head2->next = (ListNode*)malloc(sizeof(ListNode));
+    head2->next->val = 3;
+    head2->next->next = nullptr;
+    addInList(head1, head2);
+	return 0;
+}
+//[9,3,7],[6,3]
