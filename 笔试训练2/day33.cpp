68deb17f90096e1d3732c1aaff4f02dd5eb85e74
northtruths <zp1716120350@gmail.com>
2025-05-09 23:26:33 +0800

    纠错

--- 笔试训练2/day33.cpp
+++ 笔试训练2/day33.cpp
@@ -14,38 +14,35 @@
 
 
 
-//包含不超过两种字符的最长子串（未ac，70%）
+//包含不超过两种字符的最长子串（看标签、题解、搞了半天后才ac，还是细节没把控好，这次是把控不好，因为控制细节的方式选错了，类似代码框架没搞好，
+// 并且开始做时题意都没完全理解，导致开始思考的方向就有问题）
 //#include <iostream>
 //#include<string>
+//#include<unordered_map>
 //using namespace std;
 //
-//int main(){
+//int main() {
+//    //滑动窗口
+//    //维护窗口，用一个count记录当前字符种数，判断种数是否增减，用一个哈希表，当加减后变化为1、0则说明有加减
 //    string s;
 //    cin >> s;
-//    int ret = 1;
-//    int pre = 1;//前面字符串长度
-//    int i = 1;
-//    while(i < s.size()){
-//        if(s[i] != s[i - 1]){
-//            break;
+//    int left = 0, right = 0;//满足条件的子串左右
+//    int hash[26] = { 0 };
+//    int count = 0;//字符种数
+//    int ret = 0;
+//    while (right < s.size()) {
+//        if (++hash[s[right] - 'a'] == 1)
+//            ++count;
+//        if (count > 2)
+//            ret = max(ret, right - left);
+//        while (right < s.size() && count > 2) {
+//            if (--hash[s[left] - 'a'] == 0)
+//                --count;
+//            ++left;
 //        }
-//        ++i;
+//        ++right;
 //    }
-//    pre = i;
-//    ret = i;
-//    while(i < s.size()){
-//        int j = i + 1;
-//        while(j < s.size()){
-//            if(s[j] != s[j - 1])
-//                break;
-//            ++j;
-//        }
-//        int cur = j - i;
-//        ret = max(ret, pre + cur);
-//        pre = cur;
-//        i =  j;
-//    }
-//    
+//    ret = max(ret, right - left);
 //    cout << ret << endl;
 //    return 0;
 //}
